13ea53b3dd092d10c9f0de2b57bf955a19268fa4
Tansu Daylan <tansu.daylan@gmail.com>
2023-06-06 10:41:37 -0400

    NA

--- tests/test.py
+++ tests/test.py
@@ -137,18 +137,17 @@ def cnfg_FermiLAT_AGN():
     strgclus = 'FermiLAT_AGN'
     
     boolutiltpxf = False
-    #liststrgmast = ['4FGL J1800.6+7828', '4FGL J1700.0+6830', '4FGL J1821.6+6819', '4FGL J0601.1-7035', '4FGL J1748.6+7005']
-    liststrgmast = ['4FGL J1748.6+7005']
-
+    liststrgmast = ['4FGL J1800.6+7828', '4FGL J1700.0+6830', '4FGL J1821.6+6819', '4FGL J0601.1-7035', '4FGL J1748.6+7005', '3C 371']
+    #liststrgmast = ['4FGL J1748.6+7005']
+    
+    listtimescalbdtrspln = [1. / 24, 1., 5.]
     for strgmast in liststrgmast:
         
-        if strgmast == '4FGL J1806.8+6949':
-            continue
-
         miletos.main.init( \
                           strgmast=strgmast, \
                           strgclus=strgclus, \
                           boolbdtr=boolbdtr, \
+                          listtimescalbdtrspln=listtimescalbdtrspln, \
                           boolutiltpxf=boolutiltpxf, \
                           dictlygoinpt=dictlygoinpt, \
                          )
